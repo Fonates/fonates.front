7adc4d8515b0bf1b97ae78ce0ab54704c0b5b1cf
thepetruha <petrkrivosekov@gmail.com>
2024-05-02 19:01:09 +0400

    Added animation dropdown

--- src/components/DropdownMenuBurger/index.tsx
+++ src/components/DropdownMenuBurger/index.tsx
@@ -1,6 +1,7 @@
 import { useOutsideClick } from '@/hooks/useOutsideClick';
 import styles from './style.module.css'
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion, useAnimation, useCycle } from "framer-motion"
 
 interface DropdownProps {
     arrayInfo: Array<{
@@ -14,6 +15,7 @@ interface DropdownProps {
 export const Dropdown: React.FC<DropdownProps> = (props) => {
     const [isOpen, setIsOpen] = useState(false);
     const refDropdownWrapper = useRef(null);
+    const controls = useAnimation();
 
     useOutsideClick(refDropdownWrapper, () => {
         setIsOpen(false);
@@ -22,15 +24,27 @@ export const Dropdown: React.FC<DropdownProps> = (props) => {
     return (
         <div ref={refDropdownWrapper} className={styles.wp} onClick={() => setIsOpen(!isOpen)} >
             {props.children}
-            {isOpen && (
-                <div className={styles.dropdownItems}>
-                    {props.arrayInfo.map((item, index) => (
-                        <div key={index} onClick={item.onClick} className={styles.itemDropdown}>
-                            {item.name}
-                        </div>
-                    ))}
-                </div>
-            )}
+            <AnimatePresence>
+                {isOpen && (
+                    <motion.div 
+                        initial={'close'}
+                        variants={{
+                            open: { opacity: 1, y: 0 },
+                            close: { opacity: 0, y: -10 },
+                        }}
+                        exit={{ opacity: 0, y: -10 }} 
+                        animate={isOpen ? "open" : "close"}
+                        transition={{ duration: 0.2 }} 
+                        className={styles.dropdownItems}
+                    >
+                            {props.arrayInfo.map((item, index) => (
+                                <div key={index} onClick={item.onClick} className={styles.itemDropdown}>
+                                    {item.name}
+                                </div>
+                            ))}
+                    </motion.div>  
+                )}
+            </AnimatePresence>
         </div>
     )
 }
